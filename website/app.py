044cd77545e8bae30207525c175ccd7d1660c3e7
Nishchal Ravi <nxckmusicprod@gmail.com>
2024-10-09 00:20:12 +0530

    Add login pages

--- website/app.py
+++ website/app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def li():
+    return render_template('login.html')
+
+@app.route('/register')
+def reg():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000)
 
